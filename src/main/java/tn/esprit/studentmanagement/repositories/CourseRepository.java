139348ff7dcb43e7a4b86c6f7387077d43cfa37b
Waelbti <wael.benzarti@esprit.tn>
2025-12-09 21:29:10 +0100

    comment

--- src/main/java/tn/esprit/studentmanagement/repositories/CourseRepository.java
+++ src/main/java/tn/esprit/studentmanagement/repositories/CourseRepository.java
@@ -5,4 +5,6 @@ import org.springframework.stereotype.Repository;
 import tn.esprit.studentmanagement.entities.Course;
 
 @Repository
-public interface CourseRepository extends JpaRepository<Course, Long> {}
+public interface CourseRepository extends JpaRepository<Course, Long> {
+// comment
+}
